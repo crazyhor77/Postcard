695c6ae8ef6896426b33d2cf241630598242a40c
Clive Goransson <clive@The-iMac.local>
2014-12-01 17:26:18 +1100

    first rev of project

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblMessage: UILabel!
+    @IBOutlet weak var tfName: UITextField!
+    @IBOutlet weak var tfMessage: UITextField!
+    @IBOutlet weak var btnSendMail: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btnSendMail_TouchUpInside(sender: UIButton) {
+        lblMessage.hidden = false
+        lblMessage.text = tfMessage.text
+        lblMessage.textColor = UIColor.redColor()
+        
+        tfMessage.text = ""
+        tfMessage.resignFirstResponder()
+        
+        btnSendMail.setTitle("Mail Sent", forState: UIControlState.Normal)
+    }
 }
 
